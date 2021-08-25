ce79dac5b7ac7645c7736b95b902cc8f980717e0
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-25 05:18:33 +0200

    Update bing-wallpaper.ps1

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -35,7 +35,7 @@ $copyright = ($json.images.copyright)
 $urlbase = ($json.images.urlbase)
 $startdate = ($json.images.startdate)
 $shortname = ($urlbase -match '/th\?id=OHR.(.*)$') | Foreach {$Matches[1].split('_')[0]}
-$author = $copyright.split('()')[1].split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+# $author = $copyright.split('()')[1].split([IO.Path]::GetInvalidFileNameChars()) -join '_'
 $title = ($json.images.title)
 # $description = $copyright.split('()')[0]..split([IO.Path]::GetInvalidFileNameChars()) -join ''
 # $outpath = [Environment]::GetFolderPath($folderpath) + "\" + $foldername
