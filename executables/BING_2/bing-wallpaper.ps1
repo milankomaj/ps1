806824229030e1dcb213d0d51c817b4b0b30d152
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-25 04:54:37 +0200

    Update bing-wallpaper.ps1

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -21,20 +21,24 @@ $debug = $Config.Configuration.debug
 Add-Type -Assembly System.Web | Out-Null
 $BW = New-Object Net.WebClient
 $BW.Encoding = [Text.Encoding]::UTF8
-
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$notification = New-Object System.Windows.Forms.NotifyIcon 
+$notification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Get-Process -id $pid | Select-Object -ExpandProperty Path))
+if ($notification.Visible = $Config.Configuration.notification -match 'true') {
+$notification.Visible = $Config.Configuration.notification} 
 
 try
 {
-$json = ConvertFrom-Json ($BW.DownloadString('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'))
+$json = ConvertFrom-Json ($BW.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$market"))
 $url = "https://www.bing.com{0}_$resolution.jpg" -f $json.images.urlbase
-$copyright = ($json.images.copyright)
-$urlbase = ($json.images.urlbase)
-$startdate = ($json.images.startdate)
-$shortname = ($urlbase -match '/th\?id=OHR.(.*)$') | Foreach {$Matches[1].split('_')[0]}
-$author = $copyright.split('()')[1].split([IO.Path]::GetInvalidFileNameChars()) -join '_'
-$title = ($json.images.title)
-$description = $copyright.split('()')[0].split([IO.Path]::GetInvalidFileNameChars()) -join ' '
-$outpath = [Environment]::GetFolderPath($folderpath) + "\" + $foldername
+# $copyright = ($json.images.copyright)
+# $urlbase = ($json.images.urlbase)
+# $startdate = ($json.images.startdate)
+# $shortname = ($urlbase -match '/th\?id=OHR.(.*)$') | Foreach {$Matches[1].split('_')[0]}
+# $author = $copyright.split('()')[1].split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+# $title = ($json.images.title)
+# $description = $copyright.split('()')[0].split([IO.Path]::GetInvalidFileNameChars()) -join ' '
+# $outpath = [Environment]::GetFolderPath($folderpath) + "\" + $foldername
 # $ImageFileName = "$($outpath)\$($shortname)_$($startdate)_$($resolution)($($author)).jpg"
 $ImageFileName = "./executables/BING_2/latest.jpg" # github
 $TestPath = ((Test-Path -ErrorAction SilentlyContinue "$ImageFileName") -And (Get-ChildItem -ErrorAction SilentlyContinue "$ImageFileName"))
