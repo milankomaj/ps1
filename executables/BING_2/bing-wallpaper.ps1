a8b6d0e674ffbc3b243f889a305f7dfda978a616
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-25 07:51:06 +0200

    Update bing-wallpaper.ps1

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -40,7 +40,7 @@ $description = ($copyright.split([IO.Path]::GetInvalidFileNameChars()) -join ' '
 $author = ($copyright.split([IO.Path]::GetInvalidFileNameChars()) -join '_').split('©')[1]
 
 $outpath = [Environment]::GetFolderPath($folderpath) + "\" + $foldername
-$ImageFileName = "$($outpath)\$($shortname)_$($startdate)_$($resolution)('©'$($author).jpg"
+$ImageFileName = "$($outpath)\$($shortname)_$($startdate)_$($resolution)($©$author).jpg"
 # $ImageFileName = "./executables/BING_2/latest.jpg" # github
 $TestPath = ((Test-Path -ErrorAction SilentlyContinue "$ImageFileName") -And (Get-ChildItem -ErrorAction SilentlyContinue "$ImageFileName"))
 
