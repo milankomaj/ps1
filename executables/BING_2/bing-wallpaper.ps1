ca14a26e94d1f366922ec3d7d8f16852438ae1a5
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-25 04:48:18 +0200

    Update bing-wallpaper.ps1

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -21,13 +21,15 @@ $debug = $Config.Configuration.debug
 Add-Type -Assembly System.Web | Out-Null
 $BW = New-Object Net.WebClient
 $BW.Encoding = [Text.Encoding]::UTF8
-
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$notification = New-Object System.Windows.Forms.NotifyIcon 
+$notification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Get-Process -id $pid | Select-Object -ExpandProperty Path))
 if ($notification.Visible = $Config.Configuration.notification -match 'true') {
 $notification.Visible = $Config.Configuration.notification} 
 
 try
 {
-$json = ConvertFrom-Json ($BW.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$market"))
+$json = ConvertFrom-Json ($BW.DownloadString('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'))
 $url = "https://www.bing.com{0}_$resolution.jpg" -f $json.images.urlbase
 $copyright = ($json.images.copyright)
 $urlbase = ($json.images.urlbase)
