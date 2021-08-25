5d2d586dd58ab698124db46650784d99d917fc2a
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-25 08:48:07 +0200

    Update bing-wallpaper.ps1

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -91,7 +91,8 @@ $notification.Dispose() }
 catch
 {
 $ErrorMessageFull = $_.Exception.Message
-$ErrorMessage = $_.Exception.Message.split([IO.Path]::GetInvalidFileNameChars()) -join '').split(':')[2]
+
+$ErrorMessage = ($_.Exception.Message.split([IO.Path]::GetInvalidFileNameChars()) -join '').split(':')[2]
 if ($debug -match 'true') {
 Write-Output "Failed! $ErrorMessage"
 Write-Output "Failed! $ErrorMessageFull"
