86d5388b68087a452054bff904ae7e7cd6ac95c3
milankomaj <milankomaj+Github@gmail.com>
2021-12-18 13:54:54 +0100

    BING_2

--- executables/BING_2/bing-wallpaper.ps1
+++ executables/BING_2/bing-wallpaper.ps1
@@ -1,4 +1,4 @@
-#(v0.0.3)
+#(v0.0.2)
 Add-Type @"
 using System.Runtime.InteropServices;
 public class Wallpaper {
@@ -22,10 +22,10 @@ Add-Type -Assembly System.Web | Out-Null
 $BW = New-Object Net.WebClient
 $BW.Encoding = [Text.Encoding]::UTF8
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-$notification = New-Object System.Windows.Forms.NotifyIcon 
+$notification = New-Object System.Windows.Forms.NotifyIcon
 $notification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Get-Process -id $pid | Select-Object -ExpandProperty Path))
 if ($notification.Visible = $Config.Configuration.notification -match 'true') {
-$notification.Visible = $Config.Configuration.notification} 
+$notification.Visible = $Config.Configuration.notification}
 
 try
 {
@@ -34,86 +34,104 @@ $url = "https://www.bing.com{0}_$resolution.jpg" -f $json.images.urlbase
 $copyright = ($json.images.copyright)
 $urlbase = ($json.images.urlbase)
 $startdate = ($json.images.startdate)
-$shortname = ($urlbase -match '/th\?id=OHR.(.*)$') | Foreach {$Matches[1].split('_')[0]}
+$shortname = ($urlbase -match '/th\?id=OHR.(.*)$') | ForEach-Object {$Matches[1].split('_')[0]}
+$author = ($copyright.split([IO.Path]::GetInvalidFileNameChars()) -join (' ')).split('()')[1]
 $title = ($json.images.title)
 $description = ($copyright.split([IO.Path]::GetInvalidFileNameChars()) -join ' ').split('()')[0]
-$tag = Select-String '\b[A-Z]\w+,*' -CaseSensitive -input $description -AllMatches | Foreach {$_.matches}
-$author = ($copyright.split([IO.Path]::GetInvalidFileNameChars()) -join (' ')).split('()')[1]
+$tag = Select-String '\b[A-Z]\w+,*' -CaseSensitive -input $description -AllMatches | ForEach-Object {$_.matches}
 $outpath = [Environment]::GetFolderPath($folderpath) + "\" + $foldername
 $ImageFileName = "$($outpath)\$($shortname)_$($startdate)_$($resolution)($($author)).jpg"
-$TestPath = ((Test-Path -ErrorAction SilentlyContinue "$ImageFileName") -And (Get-ChildItem -ErrorAction SilentlyContinue "$ImageFileName"))
+$TestPath = ((Test-Path "$ImageFileName") -And (Get-ChildItem "$ImageFileName"))
 
 if ($debug -match 'true') {
 $request = [System.Net.WebRequest]::create($url)
 $response = $request.getResponse()
-$HTTP_Status = [int]$response.StatusCode.value__    
-Write-Host HTTP_Status:   -ForegroundColor Yellow -NoNewline; Write-Host ($HTTP_Status)  -ForegroundColor Green;
-Write-Host TestPath:      -ForegroundColor Yellow -NoNewline; Write-Host ($TestPath)     -ForegroundColor Green;
-Write-Host outpath:       -ForegroundColor Yellow -NoNewline; Write-Host($outpath)       -ForegroundColor Cyan; 
-Write-Host market:        -ForegroundColor Yellow -NoNewline; Write-Host($market)        -ForegroundColor Cyan; 
-Write-Host url:           -ForegroundColor Yellow -NoNewline; Write-Host($url)           -ForegroundColor Blue; 
-Write-Host copyright:     -ForegroundColor Yellow -NoNewline; Write-Host($copyright)     -ForegroundColor Gray;  
-Write-Host urlbase:       -ForegroundColor Yellow -NoNewline; Write-Host($urlbase)       -ForegroundColor Gray; 
-Write-Host startdate:     -ForegroundColor Yellow -NoNewline; Write-Host($startdate)     -ForegroundColor Gray;  
-Write-Host shortname:     -ForegroundColor Yellow -NoNewline; Write-Host($shortname)     -ForegroundColor Red; 
-Write-Host author:        -ForegroundColor Yellow -NoNewline; Write-Host($author)        -ForegroundColor Red; 
-Write-Host ImageFileName: -ForegroundColor Yellow -NoNewline; Write-Host($ImageFileName) -ForegroundColor Cyan;  
-Write-Host description:   -ForegroundColor Yellow -NoNewline; Write-Host($description)   -ForegroundColor Red;     
-Write-Host tag:           -ForegroundColor Yellow -NoNewline; Write-Host($tag)           -ForegroundColor Red; 
-Write-Host title:         -ForegroundColor Yellow -NoNewline; Write-Host($title)         -ForegroundColor Red; 
-}
+$HTTP_Status = [int]$response.StatusCode.value__
 
-if ($TestPath -match 'false') 
-{   
+[string]$t = $host.ui.RawUI.ForegroundColor
+[string]$host.ui.RawUI.ForegroundColor = "Green"
+[string]$host.ui.RawUI.ForegroundColor
+
+ Write-Output("HTTP_Status: $HTTP_Status")
+ Write-Output("TestPath:$TestPath")
+ Write-Output("outpath: $outpath")
+ Write-Output("metadata: $metadata")
+ Write-Output("market:$market")
+ Write-Output("url: $url")
+ Write-Output("copyright: $copyright")
+ Write-Output("urlbase: $urlbase")
+ Write-Output("startdate: $startdate")
+ Write-Output("shortname: $shortname")
+ Write-Output("author:$author ")
+ Write-Output("ImageFileName: $ImageFileName")
+ Write-Output("description: $description")
+ Write-Output("tag: $tag")
+ Write-Output("title: $title")
+ Write-Output("notification: $Config.Configuration.notification")
+
+[string]$host.ui.RawUI.ForegroundColor = $t
+[string]$host.ui.RawUI.ForegroundColor
+
+}else {}
+
+if (!$TestPath)
+{
 $BW.DownloadFile($url,$ImageFileName)
 [Wallpaper]::SetWallpaper($ImageFileName)
 
+
+
 if ($metadata -match 'true') {
 Add-Type -Path .\XperiCode.JpegMetadata.dll
-$adapter = `New-Object XperiCode.JpegMetadata.JpegMetadataAdapter(${ImageFileName})` 
-$adapter.Metadata.Title = ${copyright};
-$adapter.Metadata.Subject = ${author};
+$adapter = `New-Object XperiCode.JpegMetadata.JpegMetadataAdapter(${ImageFileName})`
+$adapter.Metadata.Title = ${copyright}
+$adapter.Metadata.Subject = ${author}
 $adapter.Metadata.Rating = $Config.Configuration.rating;
-$adapter.Metadata.Keywords.Add("${shortname}, ${tag}");
-$adapter.Metadata.Comments = ${url};
+$adapter.Metadata.Keywords.Add("${shortname}, ${tag}")
+$adapter.Metadata.Comments = ${url}
 $adapter.Save() = "SilentlyContinue"
-}
+}else {}
 
-} 
+}
 else {}
-if ($notification.Visible -match 'true') {
+
 $notification.BalloonTipIcon = "Info"
 $notification.BalloonTipText = $copyright
-$notification.BalloonTipTitle = if ($TestPath -match 'false') {"Wallpaper was downloaded and changed."} else {"Wallpaper already exist."}
+$notification.BalloonTipTitle = if (!$TestPath) {"Wallpaper was downloaded and changed."} else {"Wallpaper already exist."}
 $notification.ShowBalloonTip($timeout)
 [void][System.Threading.Thread]::Sleep($timeout)
-$notification.Dispose() }
+$notification.Dispose()
 
 }
 catch
 {
+#$_.Exception.Message.split(':')[2] -eq $null
 $ErrorMessageFull = $_.Exception.Message
-if(!($ErrorMessageFull.split(':')[2] -eq $null) -and !($ErrorMessageFull.split(':')[1] -eq $null) ) 
-{$ErrorMessage = $ErrorMessageFull.split(':')[2].split([IO.Path]::GetInvalidFileNameChars()) -join ''} 
-elseif(!($ErrorMessageFull.split(':')[1] -eq $null) -and !($ErrorMessageFull.split(':')[0] -eq $null) )
-{$ErrorMessage = $ErrorMessageFull.split(':')[1].split([IO.Path]::GetInvalidFileNameChars()) -join ''} 
-elseif(!($ErrorMessageFull.split("''")[1] -eq $null))
+if(!($null -eq ($ErrorMessageFull.split(':')[2])) -and ! ($null -eq ($ErrorMessageFull.split(':')[1])))
+{$ErrorMessage = $ErrorMessageFull.split(':')[2].split([IO.Path]::GetInvalidFileNameChars()) -join ''}
+elseif(!($null -eq ($ErrorMessageFull.split(':')[2])) -and ! ($null -eq ($ErrorMessageFull.split(':')[0])))
+{$ErrorMessage = $ErrorMessageFull.split(':')[1].split([IO.Path]::GetInvalidFileNameChars()) -join ''}
+elseif(!($null -eq ($ErrorMessageFull.split("''")[1])))
 {$ErrorMessage = $ErrorMessageFull.split("''")[1].split([IO.Path]::GetInvalidFileNameChars()) -join ''}
 else{$ErrorMessage = $ErrorMessageFull.split('.')[0].split([IO.Path]::GetInvalidFileNameChars()) -join ''}
+
 
 if ($debug -match 'true') {
 Write-Output "Failed! $ErrorMessageFull"
 Write-Output "Failed! $ErrorMessage"
-}
-if ($notification.Visible -match 'true') {
+}else {}
+
 $notification.BalloonTipIcon = "Error"
 $notification.BalloonTipText = $ErrorMessageFull
 $notification.BalloonTipTitle = $ErrorMessage
 $notification.ShowBalloonTip($timeout)
 [void][System.Threading.Thread]::Sleep($timeout)
-$notification.Dispose() }
+$notification.Dispose()
+
 exit 1
 }
+
+
 
 
 
